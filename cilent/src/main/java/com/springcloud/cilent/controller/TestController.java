352d2eb4e663e1bb4085575c9439614e6dad6391
LQQ <1246794593@qq.com>
2021-04-14 20:48:46 +0800

    message

--- cilent/src/main/java/com/springcloud/cilent/controller/TestController.java
+++ cilent/src/main/java/com/springcloud/cilent/controller/TestController.java
@@ -1,20 +1,29 @@
 package com.springcloud.cilent.controller;
 
 import com.springcloud.cilent.service.TestService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiModelProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api")
 public class TestController {
     @Autowired
     TestService testService;
-
-    @GetMapping("/test")
-    public void testApi(){
-        testService.test();
+    @ResponseBody
+    @GetMapping("selectOperation")
+    public String selectOperation(){
+        return testService.test();
     }
+    @ResponseBody
+    @PostMapping("insertOperation")
+    public void insertOperation(){}
+    @ResponseBody
+    @GetMapping("deleteOperation")
+    public void deleteOperation(){}
+    @ResponseBody
+    @PostMapping("updateOperation")
+    public void updateOperation(){}
 }
--- cilent/src/main/java/com/springcloud/cilent/dao/TestMapper.java
+++ cilent/src/main/java/com/springcloud/cilent/dao/TestMapper.java
@@ -8,4 +8,5 @@ import java.util.List;
 @Repository
 public interface TestMapper {
     List<Test> select();
+    int delete();
 }
